25625633a93265ab2079f0e16c4f7e13a2fadea6
Sensitor <nicolas.gouaux@gmail.com>
2025-12-03 17:16:00 +0100

    Child.h

--- child.h
+++ child.h
@@ -7,11 +7,25 @@
 #define WOLF__CHILD_H_
 
 typedef struct {
+  unsigned char x;
+  unsigned char y;
+  unsigned char isSafe;
+
+
   // TODO
 } Child;
+
+typedef unsigned char Map[FOREST_HEIGHT][FOREST_WIDTH];
+
+
+void isSafe (Child child);
+
+void AddVisitedPosition(Child child, Map *map);
 
 int isGameOver(GameStep step, Child child, const Wolf *wolf);
 
 void moveChildStep(const Child *child);
+
+void initChild(Child *child, unsigned char start_y, unsigned char start_x);
 
 #endif //WOLF__CHILD_H_
