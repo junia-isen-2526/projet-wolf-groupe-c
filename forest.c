1bb38b8c96d33330d134e6038df1028ea3594551
Sensitor <nicolas.gouaux@gmail.com>
2026-01-21 20:55:59 +0100

    files forest update
    correspondant aux fichiers actuels, ajout fonction isSafeZone et ValidPosition

--- forest.c
+++ forest.c
@@ -1,16 +1,51 @@
-//
-// Created by remyk on 03/12/2025.
-//
-
 #include "forest.h"
+#include <stdio.h>
+#include <string.h>
 
-Coords createCoords(unsigned char x, unsigned char y) {
-    Coords coords;
-    coords.x=x;
-    coords.y=y;
-    return coords;
+int loadForest(Forest forest, const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Erreur ouverture fichier forêt");
+        return -1;
+    }
+
+    for (int y = 0; y < FOREST_HEIGHT; y++) {
+        char buffer[FOREST_WIDTH + 10]; // Buffer avec marge
+        if (fgets(buffer, sizeof(buffer), file) == NULL) {
+            printf("Attention: forêt incomplète (ligne %d manquante)\n", y);
+            // Remplir le reste avec des espaces
+            for (int x = 0; x < FOREST_WIDTH; x++) {
+                forest[y][x] = 0;
+            }
+            continue;
+        }
+
+        // Convertir chaque caractère
+        for (int x = 0; x < FOREST_WIDTH; x++) {
+            if (x < (int)strlen(buffer) && buffer[x] == '1') {
+                forest[y][x] = 1;
+            } else {
+                forest[y][x] = 0;
+            }
+        }
+    }
+
+    fclose(file);
+    return 0;
 }
 
-void import_forest(Forest forest) {
-    //TODO
+int isValidPosition(const Forest forest, Coords coords) {
+    // Vérifier les limites
+    if (coords.x >= FOREST_WIDTH || coords.y >= FOREST_HEIGHT) {
+        return 0;
+    }
+    // Vérifier s'il n'y a pas d'arbre (0 = OK, 1 = arbre)
+    return forest[coords.y][coords.x] == 0;
 }
+
+int isSafeZone(Coords coords) {
+    // L'enfant est en sécurité sur les bordures de la forêt
+    return (coords.x == 0 || coords.x == FOREST_WIDTH - 1 ||
+            coords.y == 0 || coords.y == FOREST_HEIGHT - 1);
+}
+
--- forest.h
+++ forest.h
@@ -1,9 +1,8 @@
-//
-// Created by remyk on 03/12/2025.
-//
 
 #ifndef WOLF_FOREST_H
 #define WOLF_FOREST_H
+
+#include "coords.h"
 
 #define FOREST_WIDTH 80
 #define FOREST_HEIGHT 40
@@ -11,16 +10,14 @@
 
 typedef unsigned char Forest[FOREST_HEIGHT][FOREST_WIDTH];
 
-typedef struct Coords {
-    unsigned char y;
-    unsigned char x;
-    //il faut se souvenir que le tableau va marcher en Forest[HAUTEUR][LONGUEUR] donc la position sera child(y,x) et non child(x,y)
-} Coords;
+/* Charger la forêt depuis le fichier txt */
+int loadForest(Forest forest, const char *filename);
 
-Coords createCoords(unsigned char x, unsigned char y);
+/* Vérifier si une position est valide (pas d'arbre) */
+int isValidPosition(const Forest forest, Coords coords);
 
-void import_forest(Forest forest);
+/* Vérifier si on est en sécurité */
+int isSafeZone(Coords coords);
 
-//TODO : ça serait bien d'avoir une fonction dans Forest qui permetterait de connaitre les bords de la foret (débattable)
 
-#endif //WOLF_FOREST_H
+#endif // WOLF_FOREST_H
